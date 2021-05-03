54e00a9f9632240875a7eb33ba6b26d4216dc062
LuyoloLuja <luyolom19@gmail.com>
2021-05-03 11:43:25 +0200

    added package, classes and methods

--- src/com/tapAndGo/MakePayments.java
+++ src/com/tapAndGo/MakePayments.java
@@ -1,4 +1,12 @@
 package com.tapAndGo;
 
 public class MakePayments {
+    public static void main(String[] args) {
+        TapAndGo tapAndGoObj = new TapAndGo();
+
+        tapAndGoObj.deposit(500);
+        tapAndGoObj.pay(100);
+        tapAndGoObj.pay(600);
+        tapAndGoObj.getBalance();
+    }
 }
--- src/com/tapAndGo/TapAndGo.java
+++ src/com/tapAndGo/TapAndGo.java
@@ -4,16 +4,21 @@ public class TapAndGo {
     double accountBalance;
 
     public void pay(double amount){
-        accountBalance -= amount;
-        System.out.println("You paid, " + accountBalance);
+
+        if (amount > accountBalance) {
+            System.out.print("You have insufficient funds.");
+        } else {
+            accountBalance -= amount;
+            System.out.println("You paid : " + accountBalance);
+        }
     }
 
     public void deposit(double depositedAmount){
         accountBalance += depositedAmount;
-        System.out.print("You deposited" + depositedAmount);
+//        System.out.print("You deposited : " + depositedAmount);
     }
 
     public void getBalance() {
-        System.out.print("Your balance is " + accountBalance);
+        System.out.print("Your balance is : " + accountBalance);
     }
 }
